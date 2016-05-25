d113d27ec86113b0caf1e09e9be4b7507caa93e7
Paweł Czarnik <pawelczarnik@interia.pl>
2016-05-25 12:18:54 +0200

    done exercise3

--- test/unit/app.spec.js
+++ test/unit/app.spec.js
@@ -6,25 +6,37 @@ describe('app', function ()
 
     describe('returnOnlyLetter', function () {
         it('should return only letter', function () {
-            expect(answers.returnOnlyLetter('a4l1f5a')).toBe('al4fa');
+            expect(answers.returnOnlyLetter('a4l1f5a')).toBe('alfa');
+            expect(answers.returnOnlyLetter('a1b2bc3')).toBe('abbc');
+
         });
         it('should return empty string', function () {
-            expect(answers.returnOnlyLetter('45345')).toBe('4');
+            expect(answers.returnOnlyLetter('45345')).toBe('');
+            expect(answers.returnOnlyLetter('12345')).toBe('');
+
         });
         it('number should return false', function () {
-            expect(answers.returnOnlyLetter(45345)).toEqual(true);
+            expect(answers.returnOnlyLetter(45345)).toEqual(false);
+            expect(answers.returnOnlyLetter(123)).toEqual(false);
+
         });
     });
 
     describe('alphabetOrder', function () {
         it('should return letters in alphabetical order', function () {
             expect(answers.alphabetOrder('alfa')).toBe('aafl');
+            expect(answers.alphabetOrder('cba')).toBe('abc');
+
         });
         it('should return lower case letters in alphabetical order', function () {
             expect(answers.alphabetOrder('AlfA')).toBe('aafl');
+            expect(answers.alphabetOrder('CBA')).toBe('cba');
+
         });
         it('should return only letters in alphabetical order', function () {
-            expect(answers.alphabetOrder('A1l2f3a')).toBe('a1afl');
+            expect(answers.alphabetOrder('A1l2f3a')).toBe('aafl');
+            expect(answers.alphabetOrder('a1b2c3')).toBe('abc');
         });
      });
 });
+
